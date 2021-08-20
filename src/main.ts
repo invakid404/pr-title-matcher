deadadc6ebedee5b3aaa457068d0ddb903286e5d
invakid404 <invakid404@riseup.net>
2021-08-20 21:49:29 +0300

    feat: match title and set groups as outputs

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,28 @@
 import * as core from '@actions/core';
+import * as github from '@actions/github';
 
 (async (): Promise<void> => {
-  core.info('Hello, world!');
+  if (!github.context.payload.pull_request) {
+    core.info('No pull request found, exiting.');
+
+    return;
+  }
+
+  const { title } = github.context.payload.pull_request;
+
+  const regexPattern = core.getInput('regex', { required: true });
+  const regexFlags = core.getInput('flags');
+
+  const regex = new RegExp(regexPattern, regexFlags);
+
+  const res = regex.exec(title);
+  if (!res) {
+    core.setFailed("PR title doesn't match!");
+
+    return;
+  }
+
+  Object.entries(res.groups ?? {}).forEach(([groupName, groupVal]) =>
+    core.setOutput(groupName, groupVal),
+  );
 })();
